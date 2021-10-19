6d28c7c5d046eda07b608ec9cbf87525f856c1f7
Xu Hao <xuhao3e8@gmail.com>
2021-10-19 17:06:55 +0800

    Add swarm stuffs.

--- pos_vel_mocap/src/pos_vel_mocap.cpp
+++ pos_vel_mocap/src/pos_vel_mocap.cpp
@@ -5,6 +5,11 @@
 #include <nav_msgs/Odometry.h>
 #include <nav_msgs/Path.h>
 #include <ros/ros.h>
+#include <swarm_msgs/Pose.h>
+#include <swarm_msgs/swarm_fused.h>
+#include <swarm_msgs/swarm_fused_relative.h>
+#include <swarm_msgs/swarm_drone_basecoor.h>
+
 #define IF_SUBTRACT_INIT 1
 using namespace std;
 using namespace Eigen;
@@ -12,8 +17,12 @@ using namespace Eigen;
 ros::Publisher pub_odom;
 ros::Publisher pub_odom_slow;
 ros::Publisher pub_path;
+ros::Publisher pub_swarm_fused;
+ros::Publisher pub_swarm_drone_basecoor;
+bool enable_pub_path = false;
 
 int pose_count = 0;
+int self_id = 0;
 
 bool init_ok = false;
 
@@ -24,8 +33,27 @@ Eigen::Vector3d    Vi0, Vi1, Vi2, Vi3, Vi4, Vo0;
 
 nav_msgs::Path run_path;
 
+std::map<int, Swarm::Pose> poses_vicon;
+std::map<int, Swarm::Pose> poses_vicon_init;
+
+void pose_callback( const geometry_msgs::PoseStamped & msg, int drone_id)
+{
+    ROS_INFO_THROTTLE(1.0, "[POS_VEL] Pose from %d received", drone_id);
+    poses_vicon[drone_id] = Swarm::Pose(msg.pose);
+    if (poses_vicon_init.find(drone_id)==poses_vicon_init.end()) {
+        poses_vicon_init[drone_id] = Swarm::Pose(msg.pose);
+    }
+}
+
 void pose_callback( const geometry_msgs::PoseStamped::ConstPtr msg )
 {
+    ROS_INFO_THROTTLE(1.0, "[POS_VEL] Pose from %d received", self_id);
+    poses_vicon[self_id] = Swarm::Pose(msg->pose);
+
+    if (poses_vicon_init.find(self_id)==poses_vicon_init.end()) {
+        poses_vicon_init[self_id] = Swarm::Pose(msg->pose);
+    }
+
     if ( !init_ok )
     {
         init_ok = true;
@@ -116,83 +144,143 @@ void pose_callback( const geometry_msgs::PoseStamped::ConstPtr msg )
             pub_odom_slow.publish( odom );
         }
 
+        if (enable_pub_path) {
+            ros::Duration delta_t = now_t - last_path_t;
+            if ( delta_t.toSec() > 0.1 )
+            {
+                geometry_msgs::PoseStamped pose;
+                pose.header.stamp = now_t;
+                pose.header.frame_id = "world";
+                pose.pose.orientation.x = odom.pose.pose.orientation.x;
+                pose.pose.orientation.y = odom.pose.pose.orientation.y;
+                pose.pose.orientation.z = odom.pose.pose.orientation.z;
+                pose.pose.orientation.w = odom.pose.pose.orientation.w;
+                pose.pose.position.x = odom.pose.pose.position.x;
+                pose.pose.position.y = odom.pose.pose.position.y;
+                pose.pose.position.z = odom.pose.pose.position.z;
 
-//        ros::Duration delta_t = now_t - last_path_t;
-//        if ( delta_t.toSec() > 0.1 )
-//        {
-//            geometry_msgs::PoseStamped pose;
-//            pose.header.stamp = now_t;
-//            pose.header.frame_id = "world";
-//            pose.pose.orientation.x = odom.pose.pose.orientation.x;
-//            pose.pose.orientation.y = odom.pose.pose.orientation.y;
-//            pose.pose.orientation.z = odom.pose.pose.orientation.z;
-//            pose.pose.orientation.w = odom.pose.pose.orientation.w;
-//            pose.pose.position.x = odom.pose.pose.position.x;
-//            pose.pose.position.y = odom.pose.pose.position.y;
-//            pose.pose.position.z = odom.pose.pose.position.z;
+                run_path.header.stamp = now_t;
+                run_path.header.frame_id = "world";
+                run_path.poses.push_back( pose );
+                pub_path.publish( run_path );
 
-//            run_path.header.stamp = now_t;
-//            run_path.header.frame_id = "world";
-//            run_path.poses.push_back( pose );
-//            pub_path.publish( run_path );
-
-//            last_path_t = now_t;
-//        }
+                last_path_t = now_t;
+            }
+        }
     }
 }
 
-void vins_callback( const nav_msgs::Odometry::ConstPtr msg )
-{
-    vins_Q.w() = msg->pose.pose.orientation.w;
-    vins_Q.x() = msg->pose.pose.orientation.x;
-    vins_Q.y() = msg->pose.pose.orientation.y;
-    vins_Q.z() = msg->pose.pose.orientation.z;
+void timer_callback(const ros::TimerEvent & e) {
+    //Time to publish swarm stuffs..
+    if (poses_vicon.find(self_id)==poses_vicon.end()) {
+        ROS_INFO_THROTTLE(1.0, "[POS_VEL] timer_callback wait for %d", self_id);
+    }
 
-    Matrix3d vins_R = vins_Q.toRotationMatrix();
-    Matrix3d now_R = last_Q.toRotationMatrix();
 
-    Vector3d n = vins_R.col( 0 );
-    Vector3d o = vins_R.col( 1 );
-    Vector3d a = vins_R.col( 2 );
+    swarm_msgs::swarm_drone_basecoor basecoor;
+    auto self_init_pose = poses_vicon_init[self_id];
+    basecoor.self_id = self_id;
+    for (auto & it : poses_vicon_init) {
+        int _drone_id = it.first;
+        auto _pose = it.second;
+        auto base_coor_pose = Swarm::Pose::DeltaPose(self_init_pose, _pose, true);
+        geometry_msgs::Point p;
+        p.x = base_coor_pose.pos().x();
+        p.y = base_coor_pose.pos().y();
+        p.z = base_coor_pose.pos().z();
 
-    double vin_y = atan2( n( 1 ), n( 0 ) );
-    double vin_p = atan2( -n( 2 ), n( 0 ) * cos( vin_y ) + n( 1 ) * sin( vin_y ) );
-    double vin_r = atan2( a( 0 ) * sin( vin_y ) - a( 1 ) * cos( vin_y ),
-                          -o( 0 ) * sin( vin_y ) + o( 1 ) * cos( vin_y ) );
+        basecoor.ids.push_back(_drone_id);
+        basecoor.drone_basecoor.push_back(p);
+        basecoor.drone_baseyaw.push_back(base_coor_pose.yaw());
 
-    n = vins_R.col( 0 );
-    o = vins_R.col( 1 );
-    a = vins_R.col( 2 );
+        geometry_msgs::Vector3 pos_cov;
+        pos_cov.x = 0.01;
+        pos_cov.y = 0.01;
+        pos_cov.z = 0.01;
+        basecoor.position_cov.push_back(pos_cov);
+        basecoor.yaw_cov.push_back(0.01);
+    }
 
-    double now_y = atan2( n( 1 ), n( 0 ) );
-    double now_p = atan2( -n( 2 ), n( 0 ) * cos( now_y ) + n( 1 ) * sin( now_y ) );
-    double now_r = atan2( a( 0 ) * sin( now_y ) - a( 1 ) * cos( now_y ),
-                          -o( 0 ) * sin( now_y ) + o( 1 ) * cos( now_y ) );
+    swarm_msgs::swarm_fused swarm_fused;
 
-    std::cout << "compare\n"
-              << "now_r :" << now_r << "\nvins_r:" << vin_r
-              << "\n\n now_p :" << now_p << "\nvins_p:" << vin_p
-              << "\n\nnow_y :" << now_y << "\nvins_y:" << vin_y << "\n\n\n\n";
+    for (auto & it : poses_vicon) {
+        int _drone_id = it.first;
+        auto _pose = it.second;
+
+        //Construct swarm_fused
+        swarm_fused.ids.push_back(_drone_id);
+        geometry_msgs::Point pt;
+        pt.x = _pose.pos().x();
+        pt.y = _pose.pos().y();
+        pt.z = _pose.pos().z();
+        swarm_fused.local_drone_position.push_back(pt);
+
+        //TODO: TEMP set zero velocity
+        geometry_msgs::Vector3 vel;
+        vel.x = 0;
+        vel.y = 0;
+        vel.z = 0;
+        swarm_fused.local_drone_velocity.push_back(vel);
+        
+        geometry_msgs::Vector3 pos_cov;
+        pos_cov.x = 0.01;
+        pos_cov.y = 0.01;
+        pos_cov.z = 0.01;
+        swarm_fused.position_cov.push_back(pos_cov);
+        swarm_fused.yaw_cov.push_back(0.01);
+
+        swarm_fused.local_drone_yaw.push_back(_pose.yaw());
+    }
+
+    auto _pose = poses_vicon[self_id];
+    geometry_msgs::Point pt;
+    pt.x = _pose.pos().x();
+    pt.y = _pose.pos().y();
+    pt.z = _pose.pos().z();
+    swarm_fused.self_pos = pt;
+    swarm_fused.self_yaw = poses_vicon[self_id].yaw();
+
+    pub_swarm_fused.publish(swarm_fused);
+    pub_swarm_drone_basecoor.publish(basecoor);
 }
 
 int main( int argc, char **argv )
 {
     ros::init( argc, argv, "pos_vel_mocap" );
     ros::NodeHandle n( "~" );
-    int self_id = 0;
+    int drone_num = 0;
     n.param<int>("self_id", self_id, 1);
+    n.param<int>("drone_num", drone_num, 10);
     
     char pose_topic[100] = {0};
     sprintf(pose_topic, "/SwarmNode%d/pose", self_id);
     ROS_INFO("Subscribe pose %s", pose_topic);
+    ros::Subscriber s1 = n.subscribe(pose_topic , 100, pose_callback, ros::TransportHints().tcpNoDelay(true));
 
-    ros::Subscriber s1 = n.subscribe(pose_topic , 100, pose_callback );
-    // ros::Subscriber s2 =
-        // n.subscribe( "/vins_estimator/odometry", 10, vins_callback );
+    std::vector<ros::Subscriber> subs;
+    std::vector<boost::function<void (geometry_msgs::PoseStamped)>> cbs;
+    for (unsigned int i = 0; i < drone_num; i ++) {
+        if (i == self_id) {
+            continue;
+        }
+
+        char pose_topic[100] = {0};
+        sprintf(pose_topic, "/SwarmNode%d/pose", i);
+        ROS_INFO("Subscribe pose %s", pose_topic);
+        boost::function<void (geometry_msgs::PoseStamped)> cb = [=](geometry_msgs::PoseStamped pose) {
+                pose_callback(pose, i); 
+        };
+        cbs.push_back(cb);
+        auto hint = ros::TransportHints().tcpNoDelay(true);
+        subs.push_back(n.subscribe<geometry_msgs::PoseStamped>(pose_topic, 100, cbs.back(), ros::VoidConstPtr(), hint));
+    }
 
     pub_odom_slow = n.advertise< nav_msgs::Odometry >( "/vins_estimator/odometry", 100 );
     pub_odom = n.advertise< nav_msgs::Odometry >( "/vins_estimator/imu_propagate", 100 );
+    pub_swarm_fused = n.advertise< swarm_msgs::swarm_fused >( "/swarm_drones/swarm_drone_fused", 100 );
+    pub_swarm_drone_basecoor = n.advertise< swarm_msgs::swarm_drone_basecoor >( "/swarm_drones/swarm_drone_basecoor", 100 );
 
+    ros::Timer timer = n.createTimer(ros::Duration(0.01), timer_callback);
     ros::spin();
     return 0;
 }
